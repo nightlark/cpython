596b0c8cadd0732f62c2abf7d99ca00a0ac1a504
Ryan Mast <mast.ryan@gmail.com>
2021-08-20 20:54:41 -0700

    Try async/await

--- Lib/test/test_asyncio/test_events.py
+++ Lib/test/test_asyncio/test_events.py
@@ -1967,11 +1967,12 @@ class SubprocessTestsMixin:
 
     def test_subprocess_wait_no_same_group(self):
         # start the new process in a new session
-        connect = self.loop.subprocess_shell(
-                        functools.partial(MySubprocessProtocol, self.loop),
-                        'exit 7', stdin=None, stdout=None, stderr=None,
-                        start_new_session=True)
-        transp, proto = self.loop.run_until_complete(connect)
+        async def connect():
+            await self.loop.subprocess_shell(
+                functools.partial(MySubprocessProtocol, self.loop),
+                'exit 7', stdin=None, stdout=None, stderr=None,
+                start_new_session=True)
+        transp, proto = self.loop.run_until_complete(connect())
         self.assertIsInstance(proto, MySubprocessProtocol)
         self.loop.run_until_complete(proto.completed)
         self.assertEqual(7, proto.returncode)
