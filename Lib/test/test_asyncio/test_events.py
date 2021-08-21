df6d1b052c468c324aee7521a2d65d08499d731d
Ryan Mast <mast.ryan@gmail.com>
2021-08-20 21:08:56 -0700

    Rewrite similar to previous version with async/await

--- Lib/test/test_asyncio/test_events.py
+++ Lib/test/test_asyncio/test_events.py
@@ -1967,12 +1967,16 @@ class SubprocessTestsMixin:
 
     def test_subprocess_wait_no_same_group(self):
         # start the new process in a new session
+        proto = None
+        transp = None
         async def connect():
-            await self.loop.subprocess_shell(
+            nonlocal proto
+            nonlocal transp
+            transp, proto = await self.loop.subprocess_shell(
                 functools.partial(MySubprocessProtocol, self.loop),
                 'exit 7', stdin=None, stdout=None, stderr=None,
                 start_new_session=True)
-        transp, proto = self.loop.run_until_complete(connect())
+        self.loop.run_until_complete(connect())
         self.assertIsInstance(proto, MySubprocessProtocol)
         self.loop.run_until_complete(proto.completed)
         self.assertEqual(7, proto.returncode)
